0bca77601fc916439fbda35f83de738cf7dd6951
frank-pv <sdfsdfcdas@gmail.com>
2025-02-21 09:52:40 +0800

    add a r5c model

---
+++ model/r5c/diy-part1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Add a feed source
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+dir_pwd=`pwd`
+echo "--------------current directory is $dir_pwd --------------"
+
+#Add
+mydir='package/dev'
+mkdir -p "$mydir"
+cd $mydir
+cp $GITHUB_WORKSPACE/model/zbt-z8103ax/my-config  my-config/Makefile
+git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
+git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall-packages.git
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
+
+#argon
+sed -i 's/bing/none/' luci-app-argon-config/root/etc/config/argon
+
+#passwall
+pwdir='openwrt-passwall/luci-app-passwall/root/usr/share/passwall/rules/direct_host'
+pwpx='openwrt-passwall/luci-app-passwall/root/usr/share/passwall/rules/proxy_host'
+sed -i '/apple.com/a\bing.com' $pwdir
+sed -i '/apple.com/a\icloud.com' $pwdir
+sed -i '/bing.com/d' $pwpx
+mv openwrt-passwall/luci-app-passwall/ ./
+rm -rf  openwrt-passwall
+
+
